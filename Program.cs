c393c0ca9da8d606fc41542a72c5d6084d463cb8
Maksym Meshkov <smarty.maks13@gmail.com>
2023-06-19 23:16:14 +0200

    Test.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var startScrapping = new Parser("https://rozetka.com.ua/"); 
+        var startScrapping = new Parser("https://rozetka.com.ua/");  
         startScrapping.RunParsing();
     }
 }
